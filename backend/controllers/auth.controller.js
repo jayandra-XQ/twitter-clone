62b391c8c9e1673b41f5415a30778a70a7ef9eef
Jayandra Hamal <salonhamal278@gmail.com>
2024-05-24 12:46:19 +0530

    login functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -32,14 +32,14 @@ export const signup = async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, salt);
 
     const newUser = new User({
-			fullName,
-			username,
-			email,
-			password: hashedPassword,
-		});
+      fullName,
+      username,
+      email,
+      password: hashedPassword,
+    });
 
-    if(newUser) {
-      generateTokenAndSetCookie(newUser._id,res)
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res)
       await newUser.save();
 
       res.status(201).json({
@@ -48,25 +48,48 @@ export const signup = async (req, res) => {
         email: newUser.email,
         fullName: newUser.fullName,
         followers: newUser.followers,
-				following: newUser.following,
-				profileImg: newUser.profileImg,
-				coverImg: newUser.coverImg,
+        following: newUser.following,
+        profileImg: newUser.profileImg,
+        coverImg: newUser.coverImg,
 
       })
-    }else{
+    } else {
       return res.status(400).json({ error: "Invalid user data" });
     }
   } catch (error) {
     console.log("Error in signup controller", error.message);
-		res.status(500).json({ error: "Internal Server Error" });
+    res.status(500).json({ error: "Internal Server Error" });
 
   }
 }
 
 export const login = async (req, res) => {
-  res.json({
-    data: "You hit the login endpoint"
-  })
+  try {
+
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid credentials" })
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+    res.status(200).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      fullName: user.fullName,
+      followers: user.followers,
+      following: user.following,
+      profileImg: user.profileImg,
+      coverImg: user.coverImg,
+    })
+
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    return res.status(500).json({ error: "Internal Server Error" })
+  }
 }
 
 export const logout = async (req, res) => {
