191a022fe26f9ef2db5b594d7aed6c920f946904
Jayandra Hamal <salonhamal278@gmail.com>
2024-05-24 12:32:50 +0530

    signup functionality

---
+++ backend/lib/utils/generateToken.js
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken';
+
+export const generateTokenAndSetCookie = (userId , res) => {
+  const token = jwt.sign({userId}, process.env.JWT_SECRET, {
+    expiresIn: "15d"
+  });
+
+  res.cookie("jwt", token, {
+    maxAge: 15*24*60*60*1000, //MS
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict" //CSRF attacks cross-site request forgery attacks
+    
+
+  })
+}
--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true})); // to parse url encoded
+
 app.use("/api/auth" , authRoutes);
 
 app.listen(PORT, () => {
