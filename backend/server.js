9d5c785b5c2292a5a30e8b82c5d9b9bcb306cd12
Jayandra Hamal <salonhamal278@gmail.com>
2024-07-20 10:54:10 +0545

    1. user routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
+import {v2 as cloudinary} from "cloudinary";
 
 
 import authRoutes from './routes/auth.routes.js';
@@ -9,7 +10,13 @@ import userRoutes from './routes/user.routes.js';
 import connectMongoDB from './db/connectMongoDB.js';
 
 
-dotenv.config();
+dotenv.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+cloudinary.config
 const app = express();
 const PORT = process.env.PORT || 5000;
 
