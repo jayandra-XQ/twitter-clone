bff8a32276c85b1b1003ee697b6e58f5a54f517c
Jayandra Hamal <salonhamal278@gmail.com>
2024-07-25 23:03:20 +0545

    UI Design of X

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // change the port to 3000 from 5173
+  server: {
+    port:3000,
+  }
 })
