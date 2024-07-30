e4e95f12e0fdf886aa63b4a7a82cbefbb6216de2
Jayandra Hamal <salonhamal278@gmail.com>
2024-07-30 22:37:32 +0545

    fix: typo error

--- backend/controllers/post.controller..js
+++ backend/controllers/post.controller..js
@@ -81,7 +81,7 @@ export const commentOnPost = async (req, res) => {
 
     const comment = {user: userId, text}
 
-    post.comments.pust(comment);
+    post.comments.push(comment);
     await post.save();
     res.status(200).json(post);
   } catch (error) {
